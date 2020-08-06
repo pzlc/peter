82479cab41ae284f621a4ce8f1964ebedc0573fd
Piotrek Żygieło <pzygielo@users.noreply.github.com>
2020-08-06 11:25:16 +0200

    New upstream

--- Dockerfile.al
+++ Dockerfile.al
@@ -1,4 +1,4 @@
-FROM pzlc/payara5:v5.2020.3-2.al
+FROM pzlc/payara5:v5.2020.3-3.al
 RUN apk update
 RUN apk upgrade
 RUN rm -r -f ${PAYARAHOME}/glassfish/domains/*
--- Dockerfile.d
+++ Dockerfile.d
@@ -1,4 +1,4 @@
-FROM pzlc/payara5:v5.2020.3-2.d
+FROM pzlc/payara5:v5.2020.3-3.d
 RUN apt-get update --assume-yes
 RUN apt-get dist-upgrade --assume-yes
 RUN rm --recursive --force ${PAYARAHOME}/glassfish/domains/*
