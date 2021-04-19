691404a0272108c4a380d887232c78cf820bafba
Piotrek Żygieło <pzygielo@users.noreply.github.com>
2021-04-19 11:17:34 +0200

    New upstream

--- Dockerfile.d
+++ Dockerfile.d
@@ -1,4 +1,4 @@
-FROM pzlc/payara5:v5.2021.1-1.d
+FROM pzlc/payara5:v5.2021.2-1.d
 RUN apt-get update --assume-yes
 RUN apt-get dist-upgrade --assume-yes
 RUN rm --recursive --force ${PAYARAHOME}/glassfish/domains/*
